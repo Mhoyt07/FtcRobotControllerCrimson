6d066640ead8e4ef35a379b3f2c28e141046279f
Mhoyt07 <122686275+Mhoyt07@users.noreply.github.com>
2024-11-04 00:43:52 -0600

    added code testing subsystems

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTCSubsystemTeleOp2024.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class FTCSubsystemTeleOp2024 extends LinearOpMode {
+
+    public void runOpMode() {
+
+        waitForStart();
+        while (opModeIsActive()) {
+
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveSubsystem.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import android.view.DragEvent;
+
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class DriveSubsystem {
+    DcMotorEx back_right;
+    DcMotorEx back_left;
+    DcMotorEx front_right;
+    DcMotorEx front_left;
+    public DriveSubsystem(HardwareMap hardwareMap) {
+        //reference drive motors
+        back_right = hardwareMap.get(DcMotorEx.class, "br");
+        back_left = hardwareMap.get(DcMotorEx.class, "bl");
+
+        //reverse motors
+        back_right.setDirection(DcMotorSimple.Direction.REVERSE);
+        back_left.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+
+    }
+
+    public void arcade_drive(double rx, double ly) {
+        back_right.setPower(ly - rx);
+        back_left.setPower(ly + rx);
+    }
+}
