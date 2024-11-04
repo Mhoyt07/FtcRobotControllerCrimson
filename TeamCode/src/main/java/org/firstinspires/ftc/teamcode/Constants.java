d8dd2ff94d447ce6887de1538913651b84765769
Mhoyt07 <122686275+Mhoyt07@users.noreply.github.com>
2024-11-04 01:29:08 -0600

    created constants file

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+public class Constants {
+    //arm constants
+    //pivot constants
+    //ticks per full rotation
+    public static final int pivot_ticks = 288;
+    //pivot positions
+    public static final int pivot_pos_1 = 0;
+    public static final int pivot_pos_2 = pivot_pos_1 + pivot_ticks/4;
+    public static final int pivot_pos_3 = pivot_pos_1 + pivot_ticks/2;
+    public static final int pivot_pos_4 = pivot_pos_1 + pivot_ticks*3/4;
+}
